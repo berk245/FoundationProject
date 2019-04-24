10b9f2c055b78a73d9b8bcc2c29a2c73e7e6343f
unknown <berkozzambak@gmail.com>
2019-04-24 13:32:46 +0200

    some changes

--- app.py
+++ app.py
@@ -1,9 +1,43 @@
 from flask import Flask, render_template
+from flask_pymongo  import PyMongo
+
+
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = "test"
+app.config["MONGO_URI"] = "mongodb+srv://berkozzambak:1212qw12*@cluster0-oixaf.gcp.mongodb.net/test?retryWrites=true"
+mongo = PyMongo(app)
 
 @app.route('/')
 def clickingAround():
     return render_template("index.html")
+
+@app.route('/berlin')
+def add():
+    collection = mongo.db.foundation
+    city = collection.find()
+
+    output = ""
+
+    for venue in city:
+        map_link = "<a href='" + venue['Berlin'][2] +"'> Map Link </a>"
+        output += venue['Berlin'][0] + '  ' + venue['Berlin'][1] + map_link + '<br>'
+        
+    return output
+
+@app.route('/hamburg')
+def add1():
+    collection = mongo.db.foundation
+    city = collection.find()
+
+    output = ""
+
+    for venue in city:
+        map_link = "<a href='" + venue['Hamburg'][2] +"'> Map Link </a>"
+        output += venue['Hamburg'][0] + '  ' + venue['Hamburg'][1] + map_link + '<br>'
+        
+    return output
+
+
 
 if __name__ == "__main__":
     app.run(port='23456')
--- static/main-vue.js
+++ static/main-vue.js
@@ -1,24 +1,60 @@
-
 new Vue({
     el: '#app',
     delimiters: ["//", "//"],
     data: {
-        name: 'Welcome to the Jungle',
-        scrollPosition: null,
         par1: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
         par2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        lat: '',
+        lon: '',
+        error: '',
+        mapUrl: "",
+        locationEnabled: false,
+        userCity: "",
+      },     
+    methods: {          
+      onHover1(){
+          this.$refs.icon1.innerText = "This is the inner text";
+      },
+      onLeave1(){
+          this.$refs.icon1.innerText = "Icon 1";
+        },
+      onHover2(){
+          this.$refs.icon2.innerText = "This is the inner text";
+      },
+      onLeave2(){
+          this.$refs.icon2.innerText = "Icon 2";
+      },
+      onHover3(){
+        this.$refs.icon3.innerText = "This is the inner text";
+      },
+      onLeave3(){
+          this.$refs.icon3.innerText = "Icon 3";
+      },
+      onHover4(){
+        this.$refs.icon4.innerText = "This is the inner text";
+      },
+      onLeave4(){
+        this.$refs.icon4.innerText = "Icon 4";
+        },
+      getLocation(){
+        this.locationEnabled = !this.locationEnabled;
+        if(navigator.geolocation){
+          navigator.geolocation.getCurrentPosition(this.showPosition);
+          }
+          else{
+          this.error = "Geolocation is not supported.";   
+          }  
+        },
+        showPosition(position) {	
+           this.lat = position.coords.latitude;
+           this.lon = position.coords.longitude;
+           this.mapUrl = "https://www.openstreetmap.org/#map=18/"+this.lat+"/"+ this.lon;
 
-      },
-    
-    methods: {
-        updateScroll() {
-          this.scrollPosition = window.scrollY
-        }
-      },
-    mounted() {
-        window.addEventListener('scroll', this.updateScroll);
-      },
-    destroy() {
-        window.removeEventListener('scroll', this.updateScroll)
-      }
+           var GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + this.lat + '%2C' + this.lon + '&language=en';
+
+            this.$.getJSON(GEOCODING).done(function(location) {
+                console.log(location)
+            })
+         },
+      }  
 })
