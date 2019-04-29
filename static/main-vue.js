a8df4807aeeb28022b3620f60fc0a34ddae7ef1e
unknown <berkozzambak@gmail.com>
2019-04-29 03:06:55 +0200

    yy

--- static/main-vue.js
+++ static/main-vue.js
@@ -2,10 +2,11 @@ new Vue({
     el: '#app',
     delimiters: ["//", "//"],
     data: {
-        par1: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        par1: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
         par2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         userCity: "",
         scrollPosition: "",
+        hover: false,
 
       },     
     methods: {          
@@ -38,6 +39,9 @@ new Vue({
         if(this.scrollPosition < 630){
             this.scrollPosition = window.scrollY;
         }  
+      },
+      lowerify(){
+        this.userCity = this.userCity.toLowerCase();
       }
       },
       mounted() {
