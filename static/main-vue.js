b131ae8a42d185b5b2f79c355d71f19b0f91d5b3
unknown <berkozzambak@gmail.com>
2019-04-29 03:40:52 +0200

    l

--- static/main-vue.js
+++ static/main-vue.js
@@ -6,27 +6,26 @@ new Vue({
         par2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         userCity: "",
         scrollPosition: "",
-        hover: false,
 
       },     
     methods: {          
       onHover1(){
-          this.$refs.icon1.innerText = "This is the inner text";
+        this.$refs.icon1.innerText = "This is the inner text";
       },
       onLeave1(){
-          this.$refs.icon1.innerText = "Icon 1";
+        this.$refs.icon1.innerText = "Icon 1";
         },
       onHover2(){
-          this.$refs.icon2.innerText = "This is the inner text";
+        this.$refs.icon2.innerText = "This is the inner text";
       },
       onLeave2(){
-          this.$refs.icon2.innerText = "Icon 2";
+        this.$refs.icon2.innerText = "Icon 2";
       },
       onHover3(){
         this.$refs.icon3.innerText = "This is the inner text";
       },
       onLeave3(){
-          this.$refs.icon3.innerText = "Icon 3";
+        this.$refs.icon3.innerText = "Icon 3";
       },
       onHover4(){
         this.$refs.icon4.innerText = "This is the inner text";
@@ -45,14 +44,9 @@ new Vue({
         if(this.scrollPosition < 630){
             this.scrollPosition = window.scrollY;
         }  
-      },
-      lowerify(){
-        this.userCity = this.userCity.toLowerCase();
-      }
+        }
       },
       mounted() {
-          window.addEventListener('scroll', this.updateScroll);
-        
-        
+        window.addEventListener('scroll', this.updateScroll);
       }
 })
